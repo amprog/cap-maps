bdd3b27ed8f20a38adfa135af111f87b8b04a6c1
ameshkin <ameshkin@americanprogress.org>
2015-10-22 16:38:58 -0400

    adding starter chart json

--- wp-content/plugins/cap-graphics/assets/templates/admin/charts_new.php
+++ wp-content/plugins/cap-graphics/assets/templates/admin/charts_new.php
@@ -1,15 +1,16 @@
 <h1><?php echo $data['h1']; ?></h1>
-
+<p>Select the type of chart you want to create.  More chart types will be added shortly.</p>
 
 <div id="list_assets">
     <ul>
         <?php foreach($data['charts'] as $key=>$chart): ?>
             <li data-type="<?php echo $key; ?>" class="new_chart">
-                <h3><?php echo $chart['label']; ?></h3>
-                <img src="<?php echo $chart['img']; ?>" alt="<?php echo $chart['label']; ?>" />
+                <a href="javascript:void(0);">
+                    <h3><?php echo $chart['label']; ?></h3>
+                    <img src="<?php echo $chart['img']; ?>" alt="<?php echo $chart['label']; ?>" />
+                </a>
             </li>
         <?php endforeach; ?>
     </ul>
-
 </div>
 
